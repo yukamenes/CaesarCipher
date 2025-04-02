a57d30531167115cb8ad0b1bbcef2b58309c3893
Менес Юлія Володимирівна <your_email@example.com>
2025-04-02 17:15:11 +0300

    add scripts

---
+++ main.py
@@ -0,0 +1,32 @@
+from art import *
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+            'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    exitGame = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+    if exitGame == "no":
+        break
+
+print("Ok. See you next time !")
